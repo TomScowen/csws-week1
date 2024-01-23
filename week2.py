972c950549b47fa6d45768fe99afa0c234d86955
TomScowen <tomscowen@gmail.com>
2024-01-23 13:24:53 +0000

    macChangeWeek2

--- week2.py
+++ week2.py
@@ -1,2 +1,36 @@
-print ("test")
-print ("new edit")
+print ("Week 2 Lecture Notes")
+print ("Python Start")
+# Python Beginning
+
+# Variable
+# Any word before the =, works as a variable. Variables can also be numbers su
+my_var ="hello"
+print(my_var)
+
+
+message = "Hello, World! I like the Number 2"
+print(message)
+
+# F-String Syntax, Similar to printf() in java
+y = 1 
+x = 42
+print(f"x is equal to {x}, and y is equal to {y}.")
+
+# you can also format these at the end using .format(x,y), 
+# which does the same as before but allows easy editing at the end
+# note there is no f at the start of "", but has .format intead at the end.
+print("x is equal to {}, and y is equal to {}.".format(x,y))
+
+# NOTES When using Python datatypes do not need to be specified as python
+# will figure this out for itself. IMPORTANT
+
+# Formatting a String
+print(" ")
+
+x = 0.2+0.1
+print(str)
+str = f"{x:.2f}"
+print(str)
+
+numbers = ["one","two","three","four"]
+
